edde44839fbd7d7bfbf2f1a45a48919dbcb4eeba
seunkoko <seunowonikoko@gmail.com>
2017-01-10 12:58:35 +0100

    words function updated with tests passing

--- andelabs/library.js
+++ andelabs/library.js
@@ -4,7 +4,24 @@
 	module.exports = {
 
 		words: function (param) {
-			return "";
+			let response = {};
+			let count = 0;
+			let arrayOfWords = param.replace(/[\r\n\s\t]/g, " ").replace("  ", " ").split(" ");
+			
+			for (let i = 0; i < arrayOfWords.length; i++) {
+				let checkWord = arrayOfWords[i];
+				count = 0;
+
+				for (let j = 0; j < arrayOfWords.length; j++) {
+					if (checkWord === arrayOfWords[j]) {
+						count++;
+					}
+				}
+
+				response[checkWord] = count;
+			}
+
+			return response;
 		},
 
 		reverseString: function (param) {
