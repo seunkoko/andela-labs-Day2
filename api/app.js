01f26794ed04f97458f7a2fdfe05b9846283243d
seunkoko <seunowonikoko@gmail.com>
2017-01-10 14:48:45 +0100

    folder for api created

---
+++ api/app.js
@@ -0,0 +1,6 @@
+
+  /*
+  * This is an api that consumes football data from football-data.org
+  * It displays the league-table of popular leagues in the football
+  * world for the year 2016/2017 
+  */
